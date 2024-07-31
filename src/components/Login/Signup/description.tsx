cb47596c783fad403974af02d0c6db143c5358ba
MGavranovic <mgavranovic7@gmail.com>
2024-07-31 12:21:32 +0200

    Fixed the title and styling in the description component

--- src/components/Login/Signup/description.tsx
+++ src/components/Login/Signup/description.tsx
@@ -1,12 +1,18 @@
 function Description() {
   return (
-    <div className="container-welcome">
+    <div className="container-description">
       {/* TODO: add a catchy title */}
       {/* TODO: add a background image instead of just the color */}
-      <h1>Hello</h1>
+      <h1 className="description-title">
+        Lost in the chaos? <br /> Misplaced your notes?
+      </h1>
       <p>Trying to find a Job?</p>
       <p>Hunt for it and keep track of the applications.</p>
       <p>Keep an eye on potential future opportunities using Jaeger.</p>
+
+      <a href="#" className="description-sign-up">
+        Sign up here!
+      </a>
     </div>
   );
 }
